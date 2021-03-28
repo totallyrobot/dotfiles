1c13220bcff4a25cf268847c27ccb58cc94db8f7
TotallyRobot <level77zockthd@gmail.com>
2021-03-28 19:26:31 +0200

    changes

--- bin/rofi/archwikidocs.sh
+++ bin/rofi/archwikidocs.sh
@@ -3,7 +3,6 @@
 DMENU="rofi -dmenu -i"
 DMENU=${DMENU:-dmenu}
 
-
 FILE="$(ls /usr/share/doc/arch-wiki/html/en/ | $DMENU)"
 
 [ "$FILE" != "" ] && xdg-open "/usr/share/doc/arch-wiki/html/en/${FILE}"
--- bin/rofi/powermenu.sh
+++ bin/rofi/powermenu.sh
@@ -7,8 +7,6 @@ DMENU="rofi -dmenu -p Power Option'"
 
 output=$(echo -en "Poweroff\nReboot\nLogoff" | ${DMENU})
 
-
-
 [ "$output" = "Logoff" ] && killall i3
 [ "$output" = "Reboot" ] && $SUDO reboot
 [ "$output" = "Poweroff" ] && $SUDO poweroff
